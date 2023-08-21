2f61dc89adc73d26a4caa74e3e57cc2b7a5c3f75
Timothy <timothy.aknine@gmail.com>
2023-08-22 00:40:30 +0300

    local prono

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'JsonReader.dart'; // Fichier contenant la fonction readJson
 import 'HomePage.dart'; // Fichier contenant le widget HomePage
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
